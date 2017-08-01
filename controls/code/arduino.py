cd84eea110de53506c7f2c4a4eaf33173a3a54ab
Alexander V <yoyo.aero@yandex.ru>
2017-08-02 01:46:19 +0300

    master controller alpha

---
+++ controls/code/arduino.py
@@ -0,0 +1,13 @@
+def turnOn():
+    print("Модель включена!")
+
+def turnOff():
+    print("Модель выключена!")
+
+def getSensor(num):
+    print("Получаю значения с датчика номер {}!".format(num))
+    return 1
+
+def getEnergy():
+    print("Получаю энергию!")
+    return 100
