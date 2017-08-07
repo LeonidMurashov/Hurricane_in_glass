c3987a7de9f4426ecb5f4769cd6079fe8ecb5abb
Alexander V <yoyo.aero@yandex.ru>
2017-08-07 14:17:12 +0300

    big fixup

--- controls/code/rpiReciev.py
+++ controls/code/rpiReciev.py
@@ -1,21 +1,22 @@
 import socket
 import serial
+import random
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind(('0.0.0.0', 11719))
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+#ser = serial.Serial('/dev/ttyACM0',9600)
 
 def msgResponce(msg):
     print(msg)
-    ser.write(bytes(msg))
+    #ser.write(bytes(msg))
     if msg == "ping":
         responce = "pong"
     else:
-        responce = ser.readline()
-        #responce = "1"
+        #responce = ser.readline()
+        responce = str(random.randint(1, 65536))
     s.sendto(bytes('r: '+responce, 'utf-8'), ('255.255.255.255', 11719))
 
 
