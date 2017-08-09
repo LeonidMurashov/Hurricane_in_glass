b4b75b6dde9b9dae9b7a3f44a75471a6e4715866
Alexander V <yoyo.aero@yandex.ru>
2017-08-09 16:00:14 +0300

    some threading

--- controls/code/rpiReciev.py
+++ controls/code/rpiReciev.py
@@ -1,6 +1,8 @@
 import socket
 import serial
 import random
+from threading import Thread
+from queue import Queue
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -9,26 +11,42 @@ s.bind(('0.0.0.0', 11719))
 
 #ser = serial.Serial('/dev/ttyACM0',9600)
 
-def msgResponce(msg):
-    print(msg)
-    if msg == "ping":
-        responce = "pong"
-    elif msg.startswith("get S"):
-        responce = str(random.randint(1, 100))
-    elif msg == "get E":
-        responce = str(random.randint(1000, 3000))
-    else:
-        responce = None
-    if responce is not None:
-        s.sendto(bytes('r: '+responce, 'utf-8'), ('255.255.255.255', 11719))
+class RecieverPost(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            msg = self.queue.get()
+            msg = msg.decode('utf-8')
+            if msg == "ping":
+                responce = "pong"
+            elif msg.startswith("get S"):
+                responce = str(random.randint(1, 100))
+            elif msg == "get E":
+                responce = str(random.randint(1000, 3000))
+            else:
+                responce = None
+            if responce is not None:
+                s.sendto(bytes('r: '+responce, 'utf-8'), ('255.255.255.255', 11719))
+            self.queue.task_done()
 
 
-def getMsg():
-    while 1:
-        msg = s.recv(128)
-        msg = msg.decode('utf-8')
-        if msg.startswith("t: "):
-            msgResponce(msg[3:])
+class RecieverPre(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            msg = s.recv(128)
+            self.queue.put(msg)
 
 if __name__ == '__main__':
-    getMsg()
+    pre = RecieverPre()
+    post = RecieverPost()
+    q = Queue
+
+    pre.run(q)
+    post.run(q)
