1b9b6269c43a974b620101865ecc232b1ea6ac4c
LeonidMurashov <l.murashov@yandex.ru>
2017-08-10 11:40:05 +0000

    usb connection improoved

--- raspberry/rpiReciev.py
+++ raspberry/rpiReciev.py
@@ -11,7 +11,7 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind(('0.0.0.0', 11719))
 
-
+ser1, ser2 = None, None
 for i in range(4):
     try:
         ser1 = serial.Serial('/dev/ttyUSB'+str(i), BOD) 
@@ -24,22 +24,26 @@ for i in range(i+1,4):
         break
     except:
         continue
-if not(ser1 or ser2):
-    raise "Cannot connect USB's"
+if ser1 is None or ser2 is None:
+    raise Exception("Cannot connect USB's")
 
      
 def msgResponce(msg):
     print(msg)
-    ser1.write(bytearray(msg,'utf-8'))
-    ser2.write(bytearray(msg,'utf-8'))
-    if msg == "ping":
-        responce = "pong"
-    else:
-        responce1 = str(ser1.readline())[2:-5]
-        responce2 = str(ser2.readline())[2:-5]
-        responce = responce1 if responce2 == DUMMY else responce2 
-        print(responce1, responce2)
-        #responce = str(random.randint(1, 65536))
+    try:
+        ser1.write(bytearray(msg,'utf-8'))
+        ser2.write(bytearray(msg,'utf-8'))
+        if msg == "ping":
+            responce = "pong"
+        else:
+            responce1 = str(ser1.readline())[2:-5]
+            responce2 = str(ser2.readline())[2:-5]
+            responce = responce1 if responce2 == DUMMY else responce2 
+            print(responce1, responce2)
+            #responce = str(random.randint(1, 65536))
+    except:
+        print("Cannot connect to arduino")
+        responce = '-1'
     s.sendto(bytes('r: '+ responce, 'utf-8'), ('255.255.255.255', 11719))
 
  
