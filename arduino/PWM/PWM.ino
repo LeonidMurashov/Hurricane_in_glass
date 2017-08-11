d553fbe52516033088e578cd3d120569be3ccc04
NickTrush <nicktroosh@gmail.com>
2017-08-12 02:04:14 +0300

    PWM end, Temperature old files deleted

--- arduino/PWM/PWM.ino
+++ arduino/PWM/PWM.ino
@@ -1,26 +1,34 @@
-class TVEL
+// Класс нагрузка -- стандартный класс приборов, управляемых ШИМом
+class Load
 {
 	int pin; // Номер ШИМ-пина для ТВЭЛа
 	int power; // Мощность ТВЭЛа (0..255)
 	char msg[65]; // Сообщение приходящее от Rasbery для установки мощности
-	int len; // Длина этог сообщения
+	int len; // Длина этого сообщения
 
 	public:
-	TVEL(int pin) // Конструктор
+	// Конструктор
+	Load(int pin) 
 	{
-		this->pin = pin;
-		pinMode(pin, OUTPUT);
-		this->power = 0;
-		analogWrite(pin, power);
-		msg[0] = '\0';
+		this->pin = pin; // Устанавливаем соответсвующий пин
+		pinMode(pin, OUTPUT); // PWM требует, чтобы пин был настроен на выход
+		this->power = 0; // Ради безопасности при инициализации оставляем нагрузку выключенной
+		analogWrite(pin, power); // Запускаем режим ШИМ
+		msg[0] = '\0'; // Зануляем строку, чтобы убрать мусор
 		len = 0;
 	}
 
+	// Функция обновления режима работы по Serial-запросу
 	void Update(void)
 	{
-		if ((len = Serial.available()) > 0) // Если есть доступные данные
+		// Если есть доступные данные считываем их и только тогда изменяем режим работы
+		if ((len = Serial.available()) > 0) 
 		{
+			delay(5);
 			Serial.println(len);
+			len = Serial.available();
+			Serial.println("Updated leln = ");
+			Serial.print(len);
         	for (int i = 0; i < len; i++)
         	{
         		msg[i] = Serial.read(); // Считываем строку
@@ -40,17 +48,17 @@ class TVEL
 	}
 };
 
-TVEL TVEL_1(9);		// Объект класса TVEL на 9 пину
-TVEL TVEL_2(10);	// Объект класса TVEL на 10 пину
+Load TVEL_1(9);		// Объект класса нагрузки на 9 пину
+Load TVEL_2(10);	// Объект класса нагрузки на 10 пину
 
 void setup()
 {
 	Serial.begin(115200); // Инициализируем последовательный ввод
 }
 
+// В цикле будем постоянно спрашивать, а не нужно ли нам поменять подаваемую мощность
 void loop() 
 {
-	delay(5);
-	TVEL_1.Update();
+	TVEL_1.Update(); 
 	TVEL_2.Update();
 }
